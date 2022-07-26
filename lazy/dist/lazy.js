ba6f7fe4d02587bc040d1cc50987d6ae83320698
Sunset-kim <wood2002@naver.com>
2022-07-26 22:56:59 +0900

    intersection 옵저버를 통한 레이지 로딩

--- lazy/dist/lazy.js
+++ lazy/dist/lazy.js
@@ -1,34 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-  let active = false;
+  const lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
-  console.log(lazyImages);
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // 새로운 이미지 객체를 만든다
+        const img = new Image();
+        img.src = entry.target.dataset.src;
+        img.onload = () => {
+          entry.target.src = img.src;
+          entry.target.classList.remove("lazy");
+          observer.unobserve(entry.target);
+        };
+      }
+    });
+  });
 
-  const lazyLoad = function () {
-    if (active === false) {
-      active = true;
-      setTimeout(() => {
-        lazyImages = lazyImages
-          .map((img) => {
-            if (
-              img.getBoundingClientRect().top >= window.innerHeight &&
-              window.getComputedStyle(img).display !== "none"
-            ) {
-              img.src = img.dataset.src;
-              img.classList.remove("lazy");
-              return null;
-            } else return img;
-          })
-          .filter((img) => img);
-
-        console.log(lazyImages);
-
-        if (!lazyImages.length) {
-          document.removeEventListener("scroll", lazyLoad);
-        } else active = false;
-      }, 200);
-    }
-  };
-
-  document.addEventListener("scroll", lazyLoad);
+  lazyImages.forEach((img) => {
+    observer.observe(img);
+  });
 });

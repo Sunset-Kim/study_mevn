7de5b80f1348f8c8106399b2354a40f8e0262618
Sunset-kim <wood2002@naver.com>
2022-08-01 01:29:09 +0900

    Todo: setup 프로젝트

---
+++ vue/vue-todos/src/router/routes.ts
@@ -0,0 +1,19 @@
+import HomeViewVue from "@/views/HomeView.vue";
+import AboutViewVue from "@/views/AboutView.vue";
+
+import type { RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeViewVue,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => AboutViewVue,
+  },
+];
+
+export default routes;
---
+++ vue/vue-todos/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>안녕</main>
+</template>

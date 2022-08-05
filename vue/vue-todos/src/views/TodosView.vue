78e8386ff6223fed912e0c78fe9514aa4d53397f
Sunset-kim <wood2002@naver.com>
2022-08-06 01:52:44 +0900

    add : 명언 / 명언저장소

--- vue/vue-todos/src/views/TodosView.vue
+++ vue/vue-todos/src/views/TodosView.vue
@@ -1,9 +1,57 @@
 <template>
   <div>
-    <InputComponent />
+    <div>
+      {{ newSaying }}
+    </div>
+    <div class="input__container">
+      <InputComponent />
+    </div>
+
+    <div>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id" :class="{ todo: true, done: todo.done }">
+          <input type="checkbox" :checked="todo.done" />
+          <p>{{ todo.title }}</p>
+          <button class="button" @click="deleteTodo(todo.id)">삭제</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
-<script setup lang="ts">
+
+<style lang="scss" scoped>
+.input__container {
+  margin-top: 16px;
+}
+
+p {
+  margin-bottom: 0;
+}
+</style>
+
+<script lang="ts">
+import { onMounted } from "vue";
 import InputComponent from "../components/input/InputComponent.vue";
+import { useSaying } from "@/stores/useSaying";
+import { mapState, storeToRefs } from "pinia";
+
+export default {
+  name: "TodosView",
+  components: {
+    InputComponent,
+  },
+  setup() {
+    const today = Date().toString();
+    const saying = useSaying();
+
+    onMounted(() => {
+      saying.setSaying();
+    });
+
+    return {
+      today,
+    };
+  },
+  computed: { ...mapState(useSaying, ["newSaying"]) },
+};
 </script>
-<style lang="scss"></style>
